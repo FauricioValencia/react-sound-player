501036367d73a81a0c3723c217ab08e73de0c90e
Yesika Castillo <ylcastillo29@hotmail.com>
2021-04-04 19:39:01 -0500

    Adding mocks

---
+++ src/mocks/index.js
@@ -0,0 +1,38 @@
+export const songs = [
+  {
+    album: 'Amanecer',
+    trackName: 'Bomba Estereo - To My Love',
+    albumArtwork: '_1',
+    songsImage:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/2/2e/Amanecer_album_cover.jpg/220px-Amanecer_album_cover.jpg',
+    trackUrl: 'https://k003.kiwi6.com/hotlink/2rc3rz4rnp/1.mp3',
+  },
+  {
+    album: 'Me & You',
+    trackName: 'Alex Skrindo - Me & You',
+    albumArtwork: '_2',
+    songsImage: 'http://k003.kiwi6.com/hotlink/ifpd9xk6n4/2.jpg',
+    trackUrl: 'https://k003.kiwi6.com/hotlink/2rc3rz4rnp/1.mp3',
+  },
+  {
+    album: 'Electro Boy',
+    trackName: 'Kaaze - Electro Boy',
+    albumArtwork: '_3',
+    songsImage: 'http://k003.kiwi6.com/hotlink/36u2tfrwiu/3.jpg',
+    trackUrl: 'http://k003.kiwi6.com/hotlink/gt2rduy0mo/3.mp3',
+  },
+  {
+    album: 'Home',
+    trackName: 'Jordan Schor - Home',
+    albumArtwork: '_4',
+    songsImage: 'http://k003.kiwi6.com/hotlink/l633hnztuz/4.jpg',
+    trackUrl: 'http://k003.kiwi6.com/hotlink/421ezo6l38/4.mp3',
+  },
+  {
+    album: 'Proxy (Original Mix)',
+    trackName: 'Martin Garrix - Proxy',
+    albumArtwork: '_5',
+    songsImage: 'http://k003.kiwi6.com/hotlink/0yp24xn1o8/5.jpg',
+    trackUrl: 'http://k003.kiwi6.com/hotlink/3j1d3r8a4t/5.mp3',
+  },
+]
